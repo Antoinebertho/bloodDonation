f59b2dd0ad64c30e61cd840123b90d2121d61fbf
beuleu <billyklotz789@gmail.com>
2023-06-01 10:58:57 +0200

    router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,19 @@
 import './App.css'
 import Login from './components/Login'
 import SignUp from './components/SignUp'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 
 function App() {
 
+  const router = createBrowserRouter([
+    { path: "/", element: <> Home </> }, //TODO: Home
+    { path: "/signup", element: <SignUp/> },
+    { path: "/login", element: <Login/> }
+  ]);
+
   return (
-    <>
-    <SignUp/>
-    <Login/>
-      
-    </>
+    <RouterProvider router={router}/>
   )
 }
 
